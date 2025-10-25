daefd32ac9da95fcaf077a02e18c8eda6e4368f6
mandycs <mandycortessimon@gmail.com>
2025-10-25 20:36:23 +0200

    fix upload

--- src/MonitorEvent.cpp
+++ src/MonitorEvent.cpp
@@ -138,9 +138,19 @@ Monitor::ExecResult Monitor::handleLargeUpload(const int fdesc, const std::strin
     logger.info() << "Large upload started, received " << alreadyReceived << "/"
                   << uploadInfo.totalContentLength << " bytes initially";
 
-    // Check for Expect: 100-continue header and respond
-    if (rawRequest.find("Expect: 100-continue") != std::string::npos ||
-        rawRequest.find("Expect: 100-Continue") != std::string::npos) {
+    // Check for Expect: 100-continue header (case insensitive)
+    bool        hasExpect100Continue = false;
+    std::string lowerRequest = rawRequest;
+    for (std::size_t i = 0; i < lowerRequest.length(); ++i) {
+        if (lowerRequest[i] >= 'A' && lowerRequest[i] <= 'Z') {
+            lowerRequest[i] = lowerRequest[i] + 32;  // To lowercase
+        }
+    }
+    if (lowerRequest.find("expect: 100-continue") != std::string::npos) {
+        hasExpect100Continue = true;
+    }
+
+    if (hasExpect100Continue) {
         const char *continueResponse = "HTTP/1.1 100 Continue\r\n\r\n";
         send(fdesc, continueResponse, strlen(continueResponse), 0);
         logger.info() << "Sent 100 Continue response to client";
